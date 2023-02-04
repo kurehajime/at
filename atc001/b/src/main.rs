e722991943d9e436497a145c57e82adeff0f1048
kurehajime <xiidec@gmail.com>
2023-02-04 20:11:00 +0900

    union find

--- atc001/b/src/main.rs
+++ atc001/b/src/main.rs
@@ -1,20 +1,60 @@
 use proconio::input;
-use std::time::Instant;
+
 fn main() {
     input! {
-        n: i32,
-        q: i32,
-        querys: [[i32;3];q],
+        n: usize,
+        q: usize,
+        querys: [[usize;3];q],
     }
-    let s = Instant::now();
 
-    println!("{}", n);
-    println!("{}", q);
-    println!("{}", querys[8][2]);
-    let e = s.elapsed();
-    println!(
-        "{}.{:03}秒経過しました。",
-        e.as_secs(),
-        e.subsec_nanos() / 1_000_000
-    );
+    let mut node = vec![0; n];
+    let mut rank: Vec<usize> = vec![0; n];
+    init(&mut node);
+
+    for i in 0..q {
+        if querys[i][0] == 0 {
+            unite(&mut node, &mut rank, querys[i][1], querys[i][2]);
+        }
+        if querys[i][0] == 1 {
+            if some(&mut node, querys[i][1], querys[i][2]) {
+                println!("Yes");
+            } else {
+                println!("No");
+            }
+        }
+    }
+}
+
+fn init(node: &mut Vec<usize>) {
+    for i in 0..node.len() {
+        node[i] = i;
+    }
+}
+fn root(node: &mut Vec<usize>, x: usize) -> usize {
+    if node[x] == x {
+        x
+    } else {
+        node[x] = root(node, node[x]);
+        node[x]
+    }
+}
+fn some(node: &mut Vec<usize>, x: usize, y: usize) -> bool {
+    let rx = root(node, x);
+    let ry = root(node, y);
+    rx == ry
+}
+fn unite(node: &mut Vec<usize>, rank: &mut Vec<usize>, x: usize, y: usize) {
+    let x = root(node, x);
+    let y = root(node, y);
+    if x == y {
+        return;
+    }
+    if rank[x] < rank[y] {
+        node[x] = y;
+    } else {
+        node[y] = x;
+        if rank[x] == rank[y] {
+            rank[x] += 1;
+        }
+    }
 }
