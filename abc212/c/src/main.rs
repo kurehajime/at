39c5afcce492537440a756574fc8c9487cf30905
kurehajime <xiidec@gmail.com>
2023-02-05 21:28:33 +0900

    完成？

--- abc212/c/src/main.rs
+++ abc212/c/src/main.rs
@@ -1,22 +1,34 @@
 use proconio::input;
-use proconio::marker::Chars;
-use std::time::Instant;
 fn main() {
     input! {
         n: usize,
         m: usize,
-        narr: [usize; n],
-        marr: [usize; m],
+        narr: [isize; n],
+        marr: [isize; m],
     }
-    let s = Instant::now();
+    let marr = marr.iter().map(|x| -1 * x).collect();
+    let mut all = [narr, marr].concat();
+    all.sort_by(|a, b| a.abs().cmp(&b.abs()));
 
-    println!("{:?}", narr);
-    println!("{:?}", marr);
+    println!("{:?}", min_diff(all));
+}
+fn min_diff(all: Vec<isize>) -> isize {
+    let mut min = isize::MAX;
+    let first = all[0];
+    let mut pre_turn = first > 0;
+    let mut pre_num = first;
+    for i in 1..all.len() {
+        let turn = all[i] > 0;
+        let num = all[i];
+        if pre_turn != turn {
+            let diff = (pre_num.abs() - num.abs()).abs();
+            if diff < min {
+                min = diff;
+            }
+        }
+        pre_turn = turn;
+        pre_num = num;
+    }
 
-    let e = s.elapsed();
-    println!(
-        "{}.{:03}秒経過しました。",
-        e.as_secs(),
-        e.subsec_nanos() / 1_000_000
-    );
+    min
 }
